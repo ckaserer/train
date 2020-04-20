8e8b44bb0ebd42a3696ab572db47d3485a35b5c7
Clemens Kaserer <clemens.kaserer@gmail.com>
2020-04-20 08:44:51 +0200

    updated test script

--- .ci/test.sh
+++ .ci/test.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+# build image
 docker build -t train --no-cache . 
+
+# disable remote terraform state backend
 cat << EOF >terraform/override.tf
 provider "aws" {
   region = var.aws_region
@@ -11,10 +14,13 @@ terraform {
 }
 EOF
 
+# check terraform version
 echo "Terraform Version: "
 docker run --rm --entrypoint=/bin/terraform train version
 
+# run terraform validation
 docker run --rm -v $(pwd):/mnt/ --entrypoint=/bin/sh train -c "\
   cd /mnt/terraform && \
   terraform init -input=false && \
   terraform validate"
+
