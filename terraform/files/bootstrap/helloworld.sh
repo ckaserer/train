2a86bfc2c043f4fdb2a67cc315b87242163068a0
Clemens Kaserer <clemens.kaserer@gmail.com>
2020-04-30 14:46:41 +0200

    updated helloworld bootstrap

--- terraform/files/bootstrap/helloworld.sh
+++ terraform/files/bootstrap/helloworld.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-sudo yum update -y
-sudo yum install -y httpd
-sudo systemctl start httpd
-sudo systemctl enable httpd
+yum update -y
+yum install -y httpd
+systemctl start httpd
+systemctl enable httpd
 echo "<html><h1>Hello Terraform</h1><br>Please replace me with a proper setup script!</htlm>" > /var/www/html/index.html
