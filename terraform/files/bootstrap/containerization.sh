faeb5de2ef4a6de9d86638d81d75f62d5059cc7f
Clemens Kaserer <clemens.kaserer@gmail.com>
2020-06-24 09:32:40 +0200

    updated containerization.sh

--- terraform/files/bootstrap/containerization.sh
+++ terraform/files/bootstrap/containerization.sh
@@ -4,7 +4,8 @@ yum update -y
 yum install -y epel-release
 yum install -y ansible
 
-ansible-galaxy install gepardec.docker
+ansible-galaxy install gepardec.basics 
+ansible-galaxy install geerlingguy.docker 
 
 cat << EOF > /tmp/playbook.yml
 ---
@@ -12,11 +13,17 @@ cat << EOF > /tmp/playbook.yml
   become: true
   tasks:
     - include_role:
-        name: gepardec.docker
+        name: gepardec.basics
+      vars:
+        basics_users:
+          - centos
+        basics_bashrc: true
+    - include_role:
+        name: geerlingguy.docker
       vars: 
         docker_users:
           - centos
 ...
 EOF
 
-ansible-playbook /tmp/playbook.yml 
+ansible-playbook /tmp/playbook.yml >/tmp/playbook.out 2>&1
