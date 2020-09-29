74d1a5184af2915f9596af98ea31ea4e65ec7ebc
Clemens Kaserer <clemens.kaserer@gmail.com>
2020-09-29 18:45:49 +0200

    fixed git tag variable when building from tag

--- .ci/01_checkout_git_tag.sh
+++ .ci/01_checkout_git_tag.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-if [ "$TAG" != "latest" ]; then
+if [ "${TAG}" != "latest" ]; then
   GIT_TAG=$(git tag | tail -n${TAG} | head -n1)
   set -ex
   git checkout ${GIT_TAG}
   set +ex
+elif [ "$(git rev-parse --abbrev-ref HEAD)" == "HEAD" ]; then
+  GIT_TAG="$(git describe --tags --abbrev=0)"
+else
+  GIT_TAG="$(git rev-parse --abbrev-ref HEAD)"
 fi
