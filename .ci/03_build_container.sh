19466794cce454ffba34c4e98461c220b53dd708
Clemens Kaserer <ckaserer@gmail.com>
2021-03-09 08:15:57 +0100

    switched to github actions

--- .ci/03_build_container.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-set -x
-
-# build image
-docker build -t ${CONTAINER_IMAGE_NAME} --no-cache . 
-
-set +x
--- .ci/06_docker_push.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-set -x
-
-docker push ${CONTAINER_IMAGE_NAME}
-
-set +x
